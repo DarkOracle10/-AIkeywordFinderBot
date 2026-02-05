1b5f9b184258943b0ecf468d8a2350b751fc33e8
DarkOracle10 <amir.aeiny10@gmail.com>
2026-02-05 04:46:53 +0330

    Reorder standard library imports

--- desktop_app/main_gui.py
+++ desktop_app/main_gui.py
@@ -8,6 +8,7 @@ for searching keywords across Telegram chats.
 Version: 1.0.0
 """
 
+from datetime import datetime, timezone
 import asyncio
 import logging
 import os
@@ -15,7 +16,6 @@ import sys
 import threading
 import tkinter as tk
 import webbrowser
-from datetime import datetime, timezone
 from tkinter import messagebox, scrolledtext, ttk
 from typing import Optional
 
